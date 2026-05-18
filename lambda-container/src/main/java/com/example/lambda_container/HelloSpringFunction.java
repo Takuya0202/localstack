0f128eb18724461b53db92370d64f30d886607f3
Takuya0202 <shimozono.t.24kdgn@gmail.com>
2026-05-18 19:39:38 +0900

    fix

--- lambda-container/src/main/java/com/example/lambda_container/HelloSpringFunction.java
+++ lambda-container/src/main/java/com/example/lambda_container/HelloSpringFunction.java
@@ -3,6 +3,9 @@ package com.example.lambda_container;
 import java.util.Map;
 import java.util.function.Function;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class HelloSpringFunction implements Function<String, Map<String, String>> {
     @Override
     public Map<String, String> apply(String input) {
